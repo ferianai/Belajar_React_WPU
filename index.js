e826009dcd851bd0f112e3ee72cad948e9531efa
ferianai <ferianaisti@gmail.com>
2024-12-27 16:19:53 +0700

    menambah file untuk konfigurasi

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const container = document.getElementById('root'); // get element with id root
-const  root = ReactDOM.createRoot(container); // create root with container
+const root = ReactDOM.createRoot(container); // create root with container
 
-root.render(<h1>Belajar React bareng WPU </h1>); // render h1 element to root
+// Header component
+function Header({name}) {
+    return (
+        <h1>Belajar React bareng {name ? name : 'WPU'} </h1> // itenary operator, if name is not empty, show name, else show WPU
+    )
+}
 
-// Shortcut Ctrl+Shift+P -> "ESLint: Fix all auto-fixable problems" akan memperbaiki masalah otomatis.
+function HomePage() {
+    const students = ['Dhika', 'Budi', 'Joko', 'Rudi'];
 
+    return (
+        <div>
+            <Header name='Pak Dhika'/>
+            <ul>
+                {students.map((eachStudent) => (
+                    <li key={eachStudent}>{eachStudent}</li>
+                ))}
+            </ul>
+        </div>
+    )
+}
 
-function UnformattedComponent() {return(
-    <div> Unformatted code example with some issues.let's fix this code later.</div>
-    )}export default UnformattedComponent;
+root.render(<HomePage />); // render Header component to root
+
     
     
