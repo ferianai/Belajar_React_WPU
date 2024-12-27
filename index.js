27cb7cb92c11e187e05f5ab0d2d4ad62aa181b4a
ferianai <ferianaisti@gmail.com>
2024-12-27 15:23:06 +0700

    menambah file untuk konfigurasi

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const container = document.getElementById('root'); // get element with id root
 const  root = ReactDOM.createRoot(container); // create root with container
 
 root.render(<h1>Belajar React bareng WPU </h1>); // render h1 element to root
+
+// Shortcut Ctrl+Shift+P -> "ESLint: Fix all auto-fixable problems" akan memperbaiki masalah otomatis.
+
+
+function UnformattedComponent() {return(
+    <div> Unformatted code example with some issues.let's fix this code later.</div>
+    )}export default UnformattedComponent;
+    
+    
